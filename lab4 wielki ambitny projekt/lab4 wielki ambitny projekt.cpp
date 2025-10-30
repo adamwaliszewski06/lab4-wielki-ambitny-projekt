efaea711b83b1bf5ff24dd0c105b3ac359d66eb0
adamw06 <adam.waliszewski06@gmail.com>
2025-10-30 21:50:45 +0100

    Dodaj pliki projektów.

---
+++ lab4 wielki ambitny projekt/lab4 wielki ambitny projekt.cpp
@@ -0,0 +1,25 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+float FtoC(float stopnie); // Fahr to Celsius prototype
+float FtoK(float stopnie);
+float CtoF(float stopnie);
+float CtoK(float stopnie);
+float KtoC(float stopnie);
+float KtoF(float stopnie);
+
+int main() { 
+    float fahr, celsius;
+    for (fahr = 0.0; fahr <= 200; fahr = fahr + 20) {
+        celsius = FtoC(fahr);
+        cout << fahr << "   " << celsius << endl;
+    }
+    return 0;
+}
+
+float FtoC(float stopnie) { 
+    float wynik = (5.0 / 9.0) * (stopnie - 32.0);
+    return wynik;
+}
