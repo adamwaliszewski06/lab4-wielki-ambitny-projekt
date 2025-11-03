3f8d43ca01d8f2d431a6f05be0835f54a3e76bb9
adamw06 <adam.waliszewski06@gmail.com>
2025-11-03 18:00:50 +0100

    działające funkcje

--- lab4 wielki ambitny projekt/lab4 wielki ambitny projekt.cpp
+++ lab4 wielki ambitny projekt/lab4 wielki ambitny projekt.cpp
@@ -11,26 +11,40 @@ float KtoC(float stopnie);
 float KtoF(float stopnie);
 
 int main() { 
-    float fahr, celsius;
-    for (fahr = 0.0; fahr <= 200; fahr = fahr + 20) {
-        celsius = FtoC(fahr);
-        cout << fahr << "   " << celsius << endl;
-    }
+    float stopnie, wynik;
+    cin >> stopnie;
+    wynik = KtoF(stopnie);
+    cout << "Wynik: " << wynik;
+
+
     return 0;
 }
 
-float FtoC(float stopnie) { 
-    float c = (5.0 / 9.0) * (stopnie - 32.0);
+float FtoC(float stopnie) { //dziala
+    float wynik = (5.0 / 9.0) * (stopnie - 32.0);
     return wynik;
 }
 
-float FtoK(float stopnie) {
-    float k = (5.0 / 9.0) * (stopnie + 459.67);
+float FtoK(float stopnie) { //dziala
+    float wynik = (5.0 / 9.0) * (stopnie + 459.67);
     return wynik;
 }
-float CtoF(float stopnie) {
-    float f = (5.0 / 9.0) * stopnie + 32.0;
+float CtoF(float stopnie) { //dziala
+    float wynik = (9.0 / 5.0) * stopnie + 32.0;
     return wynik;
 }
 
-//dalej: pozostale funkcje, przetestowac je, kontynuowac kolejne polecenia
+float CtoK(float stopnie) { //dziala
+    float wynik = stopnie + 273.15;
+    return wynik;
+}
+
+float KtoC(float stopnie) { //dziala
+    float wynik = stopnie - 273.15;
+        return wynik;
+}
+
+float KtoF(float stopnie) { //dziala
+    float wynik = stopnie*(9.0/5.0)-459.67;
+    return wynik;
+}
