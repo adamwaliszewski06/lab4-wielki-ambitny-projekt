ccdc1f9cdb140e2f03438e25ec303c8df34d8058
adamw06 <adam.waliszewski06@gmail.com>
2025-11-03 18:51:21 +0100

    menu, loop

--- lab4 wielki ambitny projekt/lab4 wielki ambitny projekt.cpp
+++ lab4 wielki ambitny projekt/lab4 wielki ambitny projekt.cpp
@@ -9,57 +9,67 @@ float CtoF(float degrees);
 float CtoK(float degrees);
 float KtoC(float degrees);
 float KtoF(float degrees);
+void menu() {
+    cout << endl << "Enter a number to choose an option: " << endl;
+    cout << "1 - convert Fahrenheit to Celsius" << endl;
+    cout << "2 - convert Fahrenheit to Kelvin" << endl;
+    cout << "3 - convert Celsius to Fahrenheit" << endl;
+    cout << "4 - convert Celsius to Kelvin " << endl;
+    cout << "5 - convert Kelvin to Celsius" << endl;
+    cout << "6 - convert Kelvin to Fahrenheit" << endl;
+    cout << "7 - exit the program" << endl;
+}
 
 int main() { 
   float degrees, result;
-    int choice;
+  int choice;
+  
 
-    cout << "Enter a number to choose: " << endl << "1 - convert Fahrenheit to Celsius" << endl << "2 - convert Fahrenheit to Kelvin" << endl
-        << "3 - convert Celsius to Fahrenheit" << endl << "4 - convert Celsius to Kelvin " << endl
-        << "5 - convert Kelvin to Celsius" << endl << "6 - convert Kelvin to Fahrenheit" << endl << "7 - exit the program" << endl;
-    cin >> choice;
-
-   switch (choice)
-    {
-    case 1:
-        cout << "Enter the temperature in Fahrenheit: ";
-        cin >> degrees;
-        cout << "Temperature in Celsius: " << FtoC(degrees) << endl;
-        break;
-    case 2:
-        cout << "Enter the temperature in Fahrenheit: ";
-        cin >> degrees;
-        cout << "Temperature in Kelvin: " << FtoK(degrees) << endl;
-        break;
-    case 3:
-        cout << "Enter the temperature in Celsius: ";
-        cin >> degrees;
-        cout << "Temperature in Fahrenheit: " << CtoF(degrees) << endl;
-        break;
-    case 4:
-        cout << "Enter the temperature in Celsius: ";
-        cin >> degrees;
-        cout << "Temperature in Kelvin: " << CtoK(degrees) << endl;
-        break;
-    case 5:
-        cout << "Enter the temperature in Kelvin: ";
-        cin >> degrees;
-        cout << "Temperature in Celsius: " << KtoC(degrees) << endl;
-        break;
-    case 6:
-        cout << "Enter the temperature in Kelvin: ";
-        cin >> degrees;
-        cout << "Temperature in Fahrenheit: " << KtoF(degrees) << endl;
-        break;
-    default:
-        return 0;
-    }
-   
+  while (1) {
+      menu();
+      cin >> choice;
+      switch (choice) {
+      case 1:
+          cout << "Enter the temperature in Fahrenheit: ";
+          cin >> degrees;
+          cout << "Temperature in Celsius: " << FtoC(degrees) << endl;
+          break;
+      case 2:
+          cout << "Enter the temperature in Fahrenheit: ";
+          cin >> degrees;
+          cout << "Temperature in Kelvin: " << FtoK(degrees) << endl;
+          break;
+      case 3:
+          cout << "Enter the temperature in Celsius: ";
+          cin >> degrees;
+          cout << "Temperature in Fahrenheit: " << CtoF(degrees) << endl;
+          break;
+      case 4:
+          cout << "Enter the temperature in Celsius: ";
+          cin >> degrees;
+          cout << "Temperature in Kelvin: " << CtoK(degrees) << endl;
+          break;
+      case 5:
+          cout << "Enter the temperature in Kelvin: ";
+          cin >> degrees;
+          cout << "Temperature in Celsius: " << KtoC(degrees) << endl;
+          break;
+      case 6:
+          cout << "Enter the temperature in Kelvin: ";
+          cin >> degrees;
+          cout << "Temperature in Fahrenheit: " << KtoF(degrees) << endl;
+          break;
+      default:
+          cout << "Exiting program";
+              return 0;
+      }
+  }
   
 
 
     return 0;
 }
+
 
 float FtoC(float degrees) { //works
     float result = (5.0 / 9.0) * (degrees - 32.0);
